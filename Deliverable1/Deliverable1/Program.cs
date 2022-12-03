9fb0a5939beffb32f78f85a68bdf542f6d916bdf
Denise <drmokma@gmail.com>
2022-12-02 20:07:46 -0500

    First commit for deliverable 1

---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,44 @@
+﻿{
+    bool hungry = true;
+
+    do
+    {
+        Console.Write("How many people are we making PB&J sandwiches for?");
+        int Num = int.Parse(Console.ReadLine());
+        Decimal Val2 = Num * 2;
+        Decimal Val4 = Num * 4;
+        Decimal ValL = (Val2 / 28);
+        Decimal ValPB = (Val2 / 32);
+        Decimal ValJ = (Val4 / 48);
+
+        Console.WriteLine("You need:");
+        Console.WriteLine(" ");
+        Console.WriteLine(Num * 2 + " slices of bread");
+        Console.WriteLine(Num * 2 + " tablespoons of peanut butter");
+        Console.WriteLine(Num * 4 + " teaspoons of jelly");
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Which is...");
+        Console.WriteLine(" ");
+
+        Console.WriteLine(Math.Ceiling(ValL) + " loaves of bread");
+        Console.WriteLine(Math.Ceiling(ValPB) + " jars of peanut butter");
+        Console.WriteLine(Math.Ceiling(ValJ) + " jars of jelly");
+        Console.WriteLine(" ");
+        Console.WriteLine("Would you like to restart? Enter yes or y to continue, or enter any other key to exit.");
+
+        if (Console.ReadLine("y")) //I know this isn't fully working.
+        {
+            hungry = true; 
+        }
+        else if (Console.ReadLine("yes")) //I know this isn't fully working.
+        {
+            hungry = true;
+        }
+        else
+            Console.WriteLine("Goodbye!");
+    }
+    while (hungry);
+}
+
+
---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Welcome to our Mac & Cheese Bar. All you can eat for $9.99! We only charge extra for coffee & smoothies. How many people are in your group? Please, parties of 6 or fewer.");
+int Num = int.Parse(Console.ReadLine());
+ 
+Console.Write("A table for " + Num + "! Please follow me and take a seat. Let's get everyone started with some drinks. A water fountain and glasses are next to the plates; if you want water you can grab that. We've got smoothie (flavor of the day) or coffee.");
+Console.WriteLine(" ");
